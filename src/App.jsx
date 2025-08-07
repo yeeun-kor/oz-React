f78f318bbb51ecd4f1e918c5e1d89d94509ec3b1
yeeun <jovm268@naver.com>
2025-08-07 18:38:01 +0900

    filter()메서드 활용하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,55 @@
-import { useEffect, useState } from "react";
-
-const someVar = "test";
-const unchangedValue = "hello";
-// const로 저장 시 자동으로 변경되는지 체크
-var x = 1;
+import { useEffect, useState } from 'react';
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log(count);
-  }, []); // react-hook 경고 뜨는지 확인
+  const people = [
+    {
+      id: 0,
+      name: '라이언',
+      profession: 'mathematician',
+    },
+    {
+      id: 1,
+      name: '어피치',
+      profession: 'chemist',
+    },
+    {
+      id: 2,
+      name: '제이지',
+      profession: 'physicist',
+    },
+    {
+      id: 3,
+      name: '춘식이',
+      profession: 'chemist',
+    },
+    {
+      id: 4,
+      name: '콘',
+      profession: 'astrophysicist',
+    },
+  ];
+  const chemist = people
+    .filter((person) => person.profession === 'chemist')
+    //여기까지 하면, chemist인 객체가 나옴
+    //근데 우린 객체만 띨롱~ 나오는게 아니라, 각 객체의 요소를 꺼내서 순회할거임
+    //map으로 돌려버렷
 
-  const handleClick = () => {
-    setItems((prev) => [...prev, "new item"]);
-  };
-
+    .map((chemist) => (
+      //이렇게 map으로 돌리면
+      /* 아래처럼 띨롱 나옴!
+    우리는 여기서 이름만 뽑아서 li로 설정할것
+    이때 map으로 접근한 애는 무조건 key프로퍼티 설정하자.
+     {
+      id: 3,
+      name: "춘식이",
+      profession: "chemist",
+    }
+    */
+      <li key={chemist.id}>{chemist.name}</li>
+    ));
   return (
     <div>
-      <h1>{title}</h1>
-      <p>Count: {count}</p>
-      <div onClick={handleClick}>Click me</div>
-      <ul>
-        {items.map((item, index) => (
-          // key 누락으로 경고 확인 후 =>  <li key={index}>{item}</li> key index 오류 확인
-          <li>{item}</li>
-        ))}
-      </ul>
-      <img src="das" /> {/* 접근성 오류 뜨는지 확인 */}
+      <ul>{chemist}</ul>
     </div>
   );
 }
