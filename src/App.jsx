fbc8013a479c1658c0a0b1c4ebbf6226aa891fd0
yeeun <jovm268@naver.com>
2025-08-07 16:21:06 +0900

    uuid로 key프로퍼티 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 import "./App.css";
-import ListItem from "./ListItem";
-import { useState } from "react";
-
+import { v4 as uuidv4 } from "uuid"; //uuid설치하여서 create UUID
 function App() {
-  //useState함수 사용하기
-  const [datas, setDatas] = useState([
-    { id: "John", name: "John", age: 20 },
-    { id: "Bob", name: "Bob", age: 30 },
-    { id: "Jane", name: "Jane", age: 35 },
-    { id: "Alice", name: "Alice", age: 29 },
-  ]);
+  const datas = [
+    { id: uuidv4(), name: "John", age: 20 },
+    { id: uuidv4(), name: "Bob", age: 30 },
+    { id: uuidv4(), name: "Jane", age: 35 },
+    { id: uuidv4(), name: "Alice", age: 29 },
+  ];
 
   return (
     <div>
       {datas.map((data) => {
         return (
-          // map()메서드 안에서는 고유한 key프로퍼티값이 필요하다. 
+          // map()메서드 안에서는 고유한 key프로퍼티값이 필요하다.
           <div key={data.id}>
-            {data.name} & {data.age}
+            {data.name} & {data.age}&{data.id}
           </div>
         );
       })}
