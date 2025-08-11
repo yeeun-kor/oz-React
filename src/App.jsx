a53badd5408a41a82d5d68b59fb24992074aab38
yeeun <jovm268@naver.com>
2025-08-11 15:33:28 +0900

    1. todoList 화면 표시하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,37 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [todoList, setTodoList] = useState([
+    { id: 0, content: "양치하기" },
+    { id: 2, content: "물 마시기" },
+    { id: 3, content: "피크민 하기" },
+  ]);
+
+  return (
+    <>
+      <h1>todoList _ yeeun</h1>
+      <TodoList todoList={todoList}></TodoList>
+    </>
+  );
 }
+
+//컴포넌트 분리하기
+function TodoList({ todoList }) {
+  return (
+    <>
+      <ul>
+        {todoList.map((todo) => (
+          <List todo={todo} key={todo.id}></List>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+//리스트 컴포넌트
+function List({ todo }) {
+  return <li>{todo.content}</li>;
+}
+
 export default App;
