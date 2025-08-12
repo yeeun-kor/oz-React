ec3ffb9926416c74fd8a0c5cbb2ab8e28d0b85d8
yeeun <jovm268@naver.com>
2025-08-12 14:52:42 +0900

    Chapter04리액트 SPA - 03라우터실습하기(동물페이지)

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>💚 동물 조아 💚</h1>
+      </header>
+      <footer>all rights reserved to OZ</footer>
+    </>
+  );
+}
+
+export default App;
