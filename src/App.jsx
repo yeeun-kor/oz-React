2489d7c6019ff702c3cfe9df74de633e0ae839c2
yeeun <jovm268@naver.com>
2025-08-12 14:41:31 +0900

    Chapter04.리액트 SPA - 02라우터 컴포넌트와 라우터 함수에 대해

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,48 @@
-import { useState } from "react";
+import {
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
 import "./App.css";
 
 function App() {
-  //버튼 클릭시 기분이 바뀔 수 있도록
-  const [mood, setMood] = useState("Normal");
+  //useNavigate()
+  const navigate = useNavigate();
+  //useNavigate()
+  const location = useLocation();
+  console.log(location.pathname);
+  console.log(location.search);
+
   return (
     <>
-      <Face mood={mood}></Face> {/* 조건문에 현재 기분 상태 전달 */}
-      <div
-        className={
-          mood === "Happy" ? "happy" : mood === "Normal" ? "normal" : "sad"
-        }
-      >
-        오늘 기분 : {mood}
+      <h1>React Router예제</h1>
+      <div>
+        {/* <Link to={"/main"}>MainPage </Link>
+        <Link to={"/mypage"}>MyPage </Link>
+        <Link to={"/test"}>TestPage </Link> */}
+        <button onClick={() => navigate("/main")}>메인</button>
+        <button onClick={() => navigate("/mypage")}>마이페이지</button>
+        <button onClick={() => navigate("/test")}>테스트페이지</button>
       </div>
-      <button onClick={() => setMood("Happy")}>Happy</button>
-      <button onClick={() => setMood("Normal")}>Normal</button>
-      <button onClick={() => setMood("Sad")}>Sad</button>
-      <Face2 mood={mood}></Face2>
-      <Face3 mood={mood}></Face3>
+      <div>
+        <button onClick={() => navigate(1)}>앞으로가기</button>
+        <button onClick={() => navigate(-1)}>뒤로가기</button>
+      </div>
+      <Routes>
+        {/* main주소 뒤에 특정주소를 덧붙인다. */}
+        <Route path="/main/:hobby" element={<Main></Main>}></Route>
+        <Route path="/mypage" element={<div>MyPage🐿️</div>}></Route>
+        <Route path="/test" element={<div>TestPage🐿️</div>}></Route>
+      </Routes>
     </>
   );
 }
 
-//face컴포넌트 ( 조건에 따라 표정이 달라짐 )
-//1. if문에 따라 JSX문법 바꾸기.
-function Face({ mood }) {
-  if (mood === "Happy") {
-    return <div>😉</div>;
-  } else if (mood === "Sad") {
-    return "😭";
-  } else {
-    return "😐";
-  }
-}
-
-//2. 삼항연산자 💪🏻 가장 많이 사용
-//조건문과 달리 return 문 1개 안에서 설정하기
-function Face2({ mood }) {
-  return (
-    <>
-      {mood === "Happy" ? (
-        <div>😉</div>
-      ) : mood === "Normal" ? (
-        <div>😐</div>
-      ) : (
-        <div>😭</div>
-      )}
-    </>
-  );
-}
-
-//3. 논리연산자 💪🏻 두번째로 많이 사용
-function Face3({ mood }) {
-  return (
-    <>
-      {mood === "Happy" && <div>😉</div>}
-      {mood === "Normal" && <div>😐</div>}
-      {mood === "Sad" && <div>😭</div>}
-    </>
-  );
+function Main() {
+  const params = useParams();
+  console.log(params.hobby);
+  return <div>MainPage🐿️</div>;
 }
 export default App;
